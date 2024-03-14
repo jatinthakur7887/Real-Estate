95e36fce7a4641ba693cd210adf7f2ecb03b3117
jatinthakur7887 <jatinthakur7887@gmail.com>
2024-03-14 20:13:03 +0530

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
@@ -7,6 +8,7 @@ import SignOut from "./pages/SignOut";
 import SignUp from "./pages/SignUp";
 export default function App() {
   return <BrowserRouter>
+  <Header />
   <Routes>
     <Route path="/" element={<Home/>} />
     <Route path="/Sign-in" element={<SignIn/>} />
